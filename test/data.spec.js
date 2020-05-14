85718c9bded3eb6155f077d78542a6b45d9f9f4c
marce-8888 <mtancarao@outlook.cl>
2020-05-14 11:29:51 -0400

    tenth commit

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,74 @@
-import {filterData, filterType, filterEggs} from '../src/data.js';
+import {filterType, filterEggs} from '../src/data.js';
 
+const dataPokemon = {
+  pokemon : [
+    { "num": "004",
+      "name": "Charmander",
+      "img": "http://www.serebii.net/pokemongo/pokemon/004.png",
+      "type": [
+         "Fuego"
+      ],
+      "egg": "2 km",
+    },
+    { "num": "007",
+      "name": "Squirtle",
+      "img": "http://www.serebii.net/pokemongo/pokemon/007.png",
+      "type": [
+         "Agua"
+      ],
+      "egg": "2 km",
+    },
+    { "num": "037",
+      "name": "Vulpix",
+      "img": "http://www.serebii.net/pokemongo/pokemon/037.png",
+      "type": [
+      "Fuego"
+      ],
+      "egg": "5 km",
+    },
+  ]
+};
+
+const output = [
+  { "num": "004",
+    "name": "Charmander",
+    "img": "http://www.serebii.net/pokemongo/pokemon/004.png",
+    "type": [
+       "Fuego"
+    ],
+  },
+  { "num": "007",
+    "name": "Squirtle",
+    "img": "http://www.serebii.net/pokemongo/pokemon/007.png",
+    "type": [
+       "Agua"
+    ],
+  },
+  { "num": "037",
+    "name": "Vulpix",
+    "img": "http://www.serebii.net/pokemongo/pokemon/037.png",
+    "type": [
+       "Fuego"
+    ],
+  }
+];
 
 describe('filterType', () => {
   test('is a function', () => {
     expect(typeof filterType).toBe('function');
   });
 
-  test('returns `Roca`', () => {
-    expect(filterType()).toBe('roca');
+  it('should return Fuego', () => {
+    expect(filterType(dataPokemon, 'Fuego')).toEqual(output);
   });
 });
 
-
-describe('anotherExample', () => {
+describe('filterEggs', () => {
   test('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof filterEggs).toBe('function');
   });
 
-  test('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('should return 2 km', () => {
+    expect(filterEggs(dataPokemon, '2 km')).toEqual(output);
   });
 });
